aa31442f71abb87464b11e7bdfcd55a33f91834b
pamuduNethisa004 <pamudunj2022@gmail.com>
2026-04-16 11:20:00 +0530

    Add request and response logging filter

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,28 @@
+package com.smartcampus.filter;
+
+import javax.ws.rs.container.*;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOG = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOG.info(String.format("[REQUEST]  %s %s",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri()));
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        LOG.info(String.format("[RESPONSE] %s %s -> HTTP %d",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri(),
+                responseContext.getStatus()));
+    }
+}
