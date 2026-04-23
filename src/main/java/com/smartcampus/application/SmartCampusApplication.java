aa4bdfa156a7e5a8a9d2a29d2165f14ba2b5d66e
pamuduNethisa004 <pamudunj2022@gmail.com>
2026-04-23 18:00:04 +0530

    update SmartCampusApplication file

--- src/main/java/com/smartcampus/application/SmartCampusApplication.java
+++ src/main/java/com/smartcampus/application/SmartCampusApplication.java
@@ -1,10 +1,35 @@
 package com.smartcampus.application;
 
+import com.smartcampus.exception.*;
+import com.smartcampus.filter.LoggingFilter;
+import com.smartcampus.resource.DiscoveryResource;
+import com.smartcampus.resource.RoomResource;
+import com.smartcampus.resource.SensorResource;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api/v1")
-public class SmartCampusApplication extends Application {
-    // Jersey uses Main.java + ResourceConfig for the embedded server
-    // This class satisfies the JAX-RS Application requirement
+public class SmartCampusApplication extends ResourceConfig {
+
+    public SmartCampusApplication() {
+        // Register all resources
+        register(DiscoveryResource.class);
+        register(RoomResource.class);
+        register(SensorResource.class);
+
+        // Register exception mappers
+        register(RoomNotEmptyMapper.class);
+        register(LinkedResourceNotFoundMapper.class);
+        register(SensorUnavailableMapper.class);
+        register(NotFoundMapper.class);
+        register(GlobalExceptionMapper.class);
+
+        // Register filter
+        register(LoggingFilter.class);
+
+        // Register Jackson JSON support
+        register(JacksonFeature.class);
+    }
 }
